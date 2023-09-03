97d3f184238c6cf5b9b9a0ffe85346485e2fce35
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-03 20:35:36 +0530

    removing the error from the main.js

--- PhotoFolio/static/assets/js/main.js
+++ PhotoFolio/static/assets/js/main.js
@@ -1,3 +1,6 @@
+// import PureCounter from '../vendor/purecounter/purecounter_vanilla.js';
+
+
 /**
 * Template Name: PhotoFolio
 * Updated: Jul 27 2023 with Bootstrap v5.3.1
@@ -175,30 +178,30 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
-window.addEventListener('load', () => {
-  let portfolioContainer = select('.portfolio-container');
-  if (portfolioContainer) {
-    let portfolioIsotope = new Isotope(portfolioContainer, {
-      itemSelector: '.portfolio-item',
-      layoutMode: 'fitRows'
-    });
+// window.addEventListener('load', () => {
+//   let portfolioContainer = select('.portfolio-container');
+//   if (portfolioContainer) {
+//     let portfolioIsotope = new Isotope(portfolioContainer, {
+//       itemSelector: '.portfolio-item',
+//       layoutMode: 'fitRows'
+//     });
 
-    let portfolioFilters = select('#portfolio-flters li', true);
+//     let portfolioFilters = select('#portfolio-flters li', true);
 
-    on('click', '#portfolio-flters li', function (e) {
-      e.preventDefault();
-      portfolioFilters.forEach(function (el) {
-        el.classList.remove('filter-active');
-      });
-      this.classList.add('filter-active');
+//     on('click', '#portfolio-flters li', function (e) {
+//       e.preventDefault();
+//       portfolioFilters.forEach(function (el) {
+//         el.classList.remove('filter-active');
+//       });
+//       this.classList.add('filter-active');
 
-      portfolioIsotope.arrange({
-        filter: this.getAttribute('data-filter')
-      });
-    }, true);
-  }
+//       portfolioIsotope.arrange({
+//         filter: this.getAttribute('data-filter')
+//       });
+//     }, true);
+//   }
 
-});
+// });
 
 
 
@@ -452,6 +455,6 @@ window.addEventListener('load', () => {
   /**
    * Initiate Pure Counter 
    */
-  new PureCounter();
+  // new PureCounter();
 
 })()
