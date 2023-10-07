3cdc5a297a7ebd45cab234cbf44a26d9b21aee6b
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-10-07 15:18:58 +0530

    saving the portpolio

--- prathamesh/app.js
+++ prathamesh/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3030
 
 // Connect to MongoDB
 // mongoose.connect('mongodb://localhost:27017/restaurant', { useNewUrlParser: true, useUnifiedTopology: true })
